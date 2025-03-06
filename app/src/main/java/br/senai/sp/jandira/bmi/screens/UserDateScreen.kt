c319b0736a2b3c364a161b416afebfcddabeb100
Guilherme Felix de Araújo <24123667@dsn127m1242316.cfp127aluno.local>
2025-03-06 12:27:22 -0300

    Fhist project

--- app/src/main/java/br/senai/sp/jandira/bmi/screens/UserDateScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/UserDateScreen.kt
@@ -1,2 +1,64 @@
 package br.senai.sp.jandira.bmi.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.bmi.R
+
+@Composable
+fun UserDataScreen(modifier: Modifier = Modifier) {
+
+    Box (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        Color(0xFA500000),
+                        Color(0xFA940101),
+                        Color(0xFF910000),
+                        Color(0xFF3F0000)
+
+                    )
+                )
+            )
+    ){
+        Column (
+            modifier = modifier
+                .fillMaxSize()
+                .background(Color.Transparent)
+
+        ){
+            Text(
+                text = stringResource(
+                    R.string.next
+                ),
+                fontSize = 30.sp,
+                color = Color.White
+            )
+        }
+    }
+
+
+
+}
+
+
+
+
+
+@Preview(showSystemUi = true)
+@Composable
+private fun UserDataScreenPreview(){
+    UserDataScreen()
+}
